c6f2f920ba9e1ac70499bccb8dbcf53b8239398b
mehmetakkan <mamosakkan@gmail.com>
2024-06-27 14:01:08 +0300

    Create Root Reducer to Combine Application States
    - Implemented `rootReducer` using `combineReducers` from Redux.
    - Integrated `favoriteReducer` to manage the favorite notes feature.
    - Structured the state to include the `favorite` key for handling favorite notes.
    - Ensured that the root reducer consolidates all application state reducers for centralized state management.
    
    These changes facilitate the combination of multiple reducers into a single root reducer, enhancing state management within the application.

---
+++ src/store/rootReducer.js
@@ -0,0 +1,10 @@
+// tüm stateler burada toplandı
+
+import { combineReducers } from "redux";
+import favoriteReducer from "./reducers/favoriteReducer";
+
+const rootReducer = combineReducers({
+  favorite: favoriteReducer,
+});
+
+export default rootReducer;
