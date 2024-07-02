1425f5e3826fe777b87d368044fd252e4a76cda0
mehmetakkan <mamosakkan@gmail.com>
2024-07-02 14:45:20 +0300

    Configure environment-specific variables for frontend
    - Add .env.development for development environment
    - Add .env.production for production environment
    - Update frontend code to use environment variables for API URLs
    - Ensure .env files are ignored by Git

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export default class NoteService {
   getNotes() {
-    return axios.get("http://localhost:8080/api/notes/all");
+    return axios.get(`${API_URL}/notes/all`);
   }
   getNoteById(noteId) {
-    return axios.get(`http://localhost:8080/api/notes/${noteId}`);
+    return axios.get(`${API_URL}/notes/${noteId}`);
   }
   getNotesByCourseId(courseId) {
-    return axios.get(`http://localhost:8080/api/courses/${courseId}/notes`);
+    return axios.get(`${API_URL}/courses/${courseId}/notes`);
   }
   saveNoteWithFile(formData) {
-    return axios.post("http://localhost:8080/api/notes/create", formData, {
+    return axios.post(`${API_URL}/notes/create`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
