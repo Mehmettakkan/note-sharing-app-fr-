cdbeb53b62032c700884ca8c485f104521a7a260
mehmetakkan <mamosakkan@gmail.com>
2024-07-04 16:59:04 +0300

    changes reverted

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -4,16 +4,16 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 export default class NoteService {
   getNotes() {
-    return axios.get(`${API_URL}/notes/all`);
+    return axios.get(`${API_URL}/api/notes/all`);
   }
   getNoteById(noteId) {
-    return axios.get(`${API_URL}/notes/${noteId}`);
+    return axios.get(`${API_URL}/api/notes/${noteId}`);
   }
   getNotesByCourseId(courseId) {
-    return axios.get(`${API_URL}/courses/${courseId}/notes`);
+    return axios.get(`${API_URL}/api/courses/${courseId}/notes`);
   }
   saveNoteWithFile(formData) {
-    return axios.post(`${API_URL}/notes/create`, formData, {
+    return axios.post(`${API_URL}/api/notes/create`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
